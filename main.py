875f2a1bdd9ad81c91894f1bd680558acbf9134e
lItxXXX97 <161102877+lItxXXX97@users.noreply.github.com>
2024-06-23 16:23:23 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from utils import generate_xiaohongshu
 
 st.header("爆款小红书AI写作助手 ✏️")
 with st.sidebar:
-    openai_api_key = st.text_input("请输入OpenAI API密钥:",type="password")
+    openai_api_key = st.text_input("请输入OpenAI API密钥:", type="password")
     st.markdown("[获取OpenAI API密钥](https://platform.openai.com/account/api-keys)")
 theme = st.text_input("主题")
 submit = st.button("开始写作")
@@ -17,20 +17,13 @@ if submit and not theme:
     st.stop()
 if submit:
     with st.spinner("AI正在思考中，请稍后..."):
-        result = generate_xiaohongshu(theme,openai_api_key)
+        result = generate_xiaohongshu(theme, openai_api_key)
     st.divider()
     left_column,right_column = st.columns(2)
     with left_column:
-        st.markdown("##### 小红书标题1")
-        st.write(result.titles[0])
-        st.markdown("##### 小红书标题2")
-        st.write(result.titles[1])
-        st.markdown("##### 小红书标题3")
-        st.write(result.titles[2])
-        st.markdown("##### 小红书标题4")
-        st.write(result.titles[3])
-        st.markdown("##### 小红书标题5")
-        st.write(result.titles[4])
+        for title in result.titles:
+            st.markdown(f"##### 小红书标题{result.titles.index(title) + 1}")
+            st.write(title)
     with right_column:
         st.markdown("##### 小红书正文")
         st.write(result.content)
--- utils.py
+++ utils.py
@@ -8,13 +8,13 @@ os.environ["OPENAI_BASE_URL"] = "https://api.aigc369.com/v1"
 def generate_xiaohongshu(theme, openai_api_key):
     prompt = ChatPromptTemplate.from_messages(
         [
-            ("system",system_template_text),
-            ("user",user_template_text)
+            ("system", system_template_text),
+            ("user", user_template_text)
         ]
     )
-    model = ChatOpenAI(model="gpt-3.5-turbo", api_key= openai_api_key)
+    model = ChatOpenAI(model="gpt-3.5-turbo", api_key=openai_api_key)
     output_parser = PydanticOutputParser(pydantic_object=Xiaohonhshu)
-    chain = prompt | model| output_parser
+    chain = prompt | model | output_parser
     result = chain.invoke({
         "parser_instructions": output_parser.get_format_instructions(),
         "theme": theme
